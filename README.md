592e5218104d9374086977f9f9bf0dde52cb86fd
Atul Wagle <atul.wagle@omnicell.com>
2022-12-28 09:14:01 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,218 @@
+# This Go program is an implementation of CRM Server.
+
+## This program supports the following APIs:
+
+* Getting a single customer through a `/customers/{id}` path
+    ```curl --location --request GET 'http://localhost:3000/customers/f3f1cc7d-1f32-4652-b016-52dc3ac5bf50'
+        Response:
+        {
+            "id": "f3f1cc7d-1f32-4652-b016-52dc3ac5bf50",
+            "name": "John",
+            "role": "Supervisor",
+            "email": "john@yahoo.com",
+            "phone": 4083457834,
+            "contacted": true
+        }
+    ```
+
+* Getting all customers through a the `/customers` path
+    ```
+        curl --location --request GET 'http://localhost:3000/customers'
+        Response:
+                [
+                    {
+                        "id": "f3f1cc7d-1f32-4652-b016-52dc3ac5bf50",
+                        "name": "John",
+                        "role": "Supervisor",
+                        "email": "john@yahoo.com",
+                        "phone": 4083457834,
+                        "contacted": true
+                    },
+                    {
+                        "id": "2f7a9959-084a-41d7-a85f-5430cbf6d90a",
+                        "name": "Bill",
+                        "role": "Tester",
+                        "email": "bill@yahoo.com",
+                        "phone": 4083557844,
+                        "contacted": true
+                    },
+                    {
+                        "id": "2532aacc-a5ae-4540-a22c-d04d889f142e",
+                        "name": "Harry",
+                        "role": "Manager",
+                        "email": "harry@gmail.com",
+                        "phone": 4083657854,
+                        "contacted": false
+                    }
+                ]
+    ```
+* Creating a customer through a `/customers `path
+    ```
+        curl --location --request POST 'http://localhost:3000/customers' \
+            --header 'Content-Type: application/json' \
+            --data-raw '{
+                "name": "Atul",
+                "role": "developer",
+                "email": "atul@example.com",
+                "phone": 4084872632,
+                "contacted": true
+            }'
+        Response:
+            For successfully creating the user, API will return 201
+            {
+                "id": "137c5121-7aff-4ae5-9dd6-da4f562c9a21",
+                "name": "Atul",
+                "role": "developer",
+                "email": "atul@example.com",
+                "phone": 0,
+                "contacted": true
+            }
+
+            In case the same payload is submitted once again, API will return 409 and 
+            Record already present.
+    ```
+* Updating a customer through a `/customers/{id} `path
+    ```
+    This API support both PATCH and PUT http methods. 
+
+        curl --location --request PATCH 'http://localhost:3000/customers/137c5121-7aff-4ae5-9dd6-da4f562c9a21' 
+            --header 'Content-Type: application/json'
+            --data-raw '{
+                "name": "Atul",
+                "role": "developer",
+                "email": "atul@gmail.com",
+                "phone": 4084872655,
+                "contacted": false
+            }'
+
+        Response:
+            If the resource is properly updated the API returns 200 and the updated resource:
+                {
+                    "id": "137c5121-7aff-4ae5-9dd6-da4f562c9a21",
+                    "name": "Atul",
+                    "role": "developer",
+                    "email": "atul@gmail.com",
+                    "phone": 4084872655,
+                    "contacted": false
+                }
+
+            If the ID is not associated with a resource then API return 404 and Record not found.
+    ```
+* Updating multiple customer through a `/customers` path
+    ```
+        This API support both PATCH and PUT http methods.
+        Using this API user can update multiple resources.
+        In the response of the API, there will be a http status associated with every customer entry.
+
+            curl --location --request PATCH 'http://localhost:3000/customers' \
+                --header 'Content-Type: application/json' \
+                --data-raw '[
+                    {
+                        "id": "f3f1cc7d-1f32-4652-b016-52dc3ac5bf50",
+                        "name": "John",
+                        "role": "Supervisor",
+                        "email": "john@yahoo.com",
+                        "phone": 4083457834,
+                        "contacted": true
+                    },
+                    {
+                        "id": "2f7a9959-084a-41d7-a85f-5430cbf6d90a",
+                        "name": "Bill",
+                        "role": "Tester",
+                        "email": "bill@yahoo.com",
+                        "phone": 4083557844,
+                        "contacted": true
+                    }
+                ]'
+
+            Response:
+                If the API gets executed without any exceptions then it will return 200.
+                    [
+                        {
+                            "customerentry": {
+                                "id": "f3f1cc7d-1f32-4652-b016-52dc3ac5bf50",
+                                "name": "John",
+                                "role": "Supervisor",
+                                "email": "john@yahoo.com",
+                                "phone": 4083457834,
+                                "contacted": true
+                            },
+                            "status": 200
+                        },
+                        {
+                            "customerentry": {
+                                "id": "2f7a9959-084a-41d7-a85f-5430cbf6d90a",
+                                "name": "Bill",
+                                "role": "Tester",
+                                "email": "bill@yahoo.com",
+                                "phone": 4083557844,
+                                "contacted": true
+                            },
+                            "status": 200
+                        }
+                    ]
+                
+                If any resource could not be updated/found, API will return response such as:
+
+                    [
+                        {
+                            "customerentry": {
+                                "id": "f3f1cc7d-1f32-4652-b016-52dc3ac5bggg",
+                                "name": "John",
+                                "role": "Supervisor",
+                                "email": "john@yahoo.com",
+                                "phone": 4083457834,
+                                "contacted": true
+                            },
+                            "status": 404
+                        },
+                        {
+                            "customerentry": {
+                                "id": "2f7a9959-084a-41d7-a85f-5430cbf6d90a",
+                                "name": "Bill",
+                                "role": "Tester",
+                                "email": "bill@yahoo.com",
+                                "phone": 4083557844,
+                                "contacted": true
+                            },
+                            "status": 200
+                        }
+                    ]
+
+    ```
+* Deleting a customer through a `/customers/{id}` path
+    ```
+        curl --location --request DELETE 'http://localhost:3000/customers/f3f1cc7d-1f32-4652-b016-52dc3ac5bf50'
+
+        Response:
+            If the API is able to delete the resource it return 200 and contents of the customers db.
+
+            [
+                {
+                    "id": "2f7a9959-084a-41d7-a85f-5430cbf6d90a",
+                    "name": "Bill",
+                    "role": "Tester",
+                    "email": "bill@yahoo.com",
+                    "phone": 4083557844,
+                    "contacted": true
+                },
+                {
+                    "id": "2532aacc-a5ae-4540-a22c-d04d889f142e",
+                    "name": "Harry",
+                    "role": "Manager",
+                    "email": "harry@gmail.com",
+                    "phone": 4083657854,
+                    "contacted": false
+                },
+                {
+                    "id": "0ea8f336-7c45-4d81-a17d-b736b1ca4d28",
+                    "name": "Atul",
+                    "role": "developer",
+                    "email": "atul@example.com",
+                    "phone": 4084872632,
+                    "contacted": true
+                }
+            ]
+
+            If we try to delete the same resource once again or try to delete using an invalid id, then the API returns 404 and Record not found.
+    ```
---
+++ crmserver.go
@@ -0,0 +1,275 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+)
+
+type Customer struct {
+	Id        uuid.UUID `json:"id"`
+	Name      string    `json:"name"`
+	Role      string    `json:"role"`
+	Email     string    `json:"email"`
+	Phone     int       `json:"phone"`
+	Contacted bool      `json:"contacted"`
+}
+
+type ErrorResponse struct {
+	Errormessage string `json:"errormessage"`
+}
+
+var uuid_1, error1 = uuid.Parse("f3f1cc7d-1f32-4652-b016-52dc3ac5bf50")
+var uuid_2, error2 = uuid.Parse("2f7a9959-084a-41d7-a85f-5430cbf6d90a")
+var uuid_3, error3 = uuid.Parse("2532aacc-a5ae-4540-a22c-d04d889f142e")
+
+var customer1 = Customer{uuid_1, "John", "Supervisor", "john@gmail.com", 4083457834, false}
+var customer2 = Customer{uuid_2, "Bill", "Tester", "bill@gmail.com", 4083557844, false}
+var customer3 = Customer{uuid_3, "Harry", "Manager", "harry@gmail.com", 4083657854, false}
+
+var customerDb = []Customer{customer1, customer2, customer3}
+
+func getCustomers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(customerDb)
+}
+
+func getCustomer(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var tempCustomer = new(Customer)
+
+	params := mux.Vars(r)
+	var customerid = params["id"]
+	if len(strings.TrimSpace(customerid)) == 0 {
+		senderrormessage(w, "Invalid customer id.", http.StatusBadRequest)
+	} else {
+		var found bool
+		for i := 0; i < len(customerDb); i++ {
+			if customerDb[i].Id.String() == customerid {
+				tempCustomer.Id = customerDb[i].Id
+				tempCustomer.Contacted = customerDb[i].Contacted
+				tempCustomer.Email = customerDb[i].Email
+				tempCustomer.Name = customerDb[i].Name
+				tempCustomer.Phone = customerDb[i].Phone
+				tempCustomer.Role = customerDb[i].Role
+				w.WriteHeader(http.StatusOK)
+				json.NewEncoder(w).Encode(tempCustomer)
+				found = true
+				break
+			}
+		}
+		if !found {
+			senderrormessage(w, "Record not found.", http.StatusNotFound)
+		}
+	}
+}
+
+func deleteCustomer(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+	var customerid = params["id"]
+	if len(strings.TrimSpace(customerid)) == 0 {
+		senderrormessage(w, "Invalid customer id.", http.StatusBadRequest)
+	} else {
+		var found bool
+		for i := 0; i < len(customerDb); i++ {
+			if customerDb[i].Id.String() == customerid {
+				customerDb = append(customerDb[:i], customerDb[i+1:]...)
+				w.WriteHeader(http.StatusOK)
+				json.NewEncoder(w).Encode(customerDb)
+				found = true
+				break
+			}
+		}
+		if !found {
+			senderrormessage(w, "Record not found.", http.StatusNotFound)
+		}
+	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	fmt.Fprintf(w, "<html><body>")
+	fmt.Fprintf(w, "<h1>List of supported APIs:</h1>\n")
+	fmt.Fprintf(w, "<ul>")
+	fmt.Fprintf(w, "<li>Getting a single customer through a /customers/{id} path</li>")
+	fmt.Fprintf(w, "<li>Getting all customers through a the /customers path</li>")
+	fmt.Fprintf(w, "<li>Creating a customer through a /customers path</li>")
+	fmt.Fprintf(w, "<li>Updating a customer through a /customers/{id} path</li>")
+	fmt.Fprintf(w, "<li>Deleting a customer through a /customers/{id} path</li>")
+	fmt.Fprintf(w, "</ul>")
+	fmt.Fprintf(w, "</body></html>")
+}
+
+func addCustomer(w http.ResponseWriter, r *http.Request) {
+	// Set the appropriate Content-Type in the response header
+	w.Header().Set("Content-Type", "application/json")
+
+	var newCustomerEntry = Customer{Id: uuid.New(), Name: "", Role: "", Email: "", Phone: 0, Contacted: false}
+
+	// Read the HTTP request body
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Printf("AddCustomer: Error during reading the request body: %v\n", err)
+	}
+
+	// Encode the request body into a Golang value so that we can work with the data
+	err = json.Unmarshal(reqBody, &newCustomerEntry)
+	if err != nil {
+		fmt.Printf("AddCustomer: Error during unmarshalling: %v\n", err)
+	}
+
+	var found bool
+	for i := 0; i < len(customerDb); i++ {
+		if customerDb[i].Contacted == newCustomerEntry.Contacted &&
+			customerDb[i].Email == newCustomerEntry.Email &&
+			customerDb[i].Name == newCustomerEntry.Name &&
+			customerDb[i].Role == newCustomerEntry.Role &&
+			customerDb[i].Phone == newCustomerEntry.Phone {
+			found = true
+
+			senderrormessage(w, "Record already present.", http.StatusNotFound)
+
+			break
+		}
+	}
+
+	if !found {
+		customerDb = append(customerDb, newCustomerEntry)
+		w.WriteHeader(http.StatusCreated)
+		// Regardless of successful resource creation or not, return the newly added customer record
+		json.NewEncoder(w).Encode(newCustomerEntry)
+	}
+}
+
+func updateCustomer(w http.ResponseWriter, r *http.Request) {
+	// Set the appropriate Content-Type in the response header
+	w.Header().Set("Content-Type", "application/json")
+
+	var updCustomerEntry = new(Customer)
+
+	params := mux.Vars(r)
+	var customerid = params["id"]
+	if len(strings.TrimSpace(customerid)) == 0 {
+		senderrormessage(w, "Invalid customer id.", http.StatusBadRequest)
+	} else {
+		// Read the HTTP request body
+		reqBody, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			fmt.Printf("UpdateCustomer: Error during reading the request body: %v\n", err)
+		}
+
+		// Encode the request body into a Golang value so that we can work with the data
+		err = json.Unmarshal(reqBody, &updCustomerEntry)
+		if err != nil {
+			fmt.Printf("UpdateCustomer: Error during unmarshalling: %v\n", err)
+		}
+
+		var found bool
+		for i := 0; i < len(customerDb); i++ {
+			if customerDb[i].Id.String() == customerid {
+				customerDb[i].Contacted = updCustomerEntry.Contacted
+				customerDb[i].Email = updCustomerEntry.Email
+				customerDb[i].Name = updCustomerEntry.Name
+				customerDb[i].Role = updCustomerEntry.Role
+				customerDb[i].Phone = updCustomerEntry.Phone
+				found = true
+				w.WriteHeader(http.StatusOK)
+				json.NewEncoder(w).Encode(customerDb[i])
+				break
+			}
+		}
+
+		if !found {
+			senderrormessage(w, "Record not found.", http.StatusNotFound)
+		}
+	}
+}
+
+func updateCustomers(w http.ResponseWriter, r *http.Request) {
+	// Set the appropriate Content-Type in the response header
+	w.Header().Set("Content-Type", "application/json")
+
+	type UpdCustomersResponse struct {
+		Customerentry Customer `json:"customerentry"`
+		Statuscode    int      `json:"status"`
+	}
+
+	var updCustomerEntries []Customer
+
+	// Read the HTTP request body
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Printf("UpdateCustomers: Error during reading the request body: %v\n", err)
+	}
+
+	// Encode the request body into a Golang value so that we can work with the data
+	err = json.Unmarshal(reqBody, &updCustomerEntries)
+	if err != nil {
+		fmt.Printf("UpdateCustomers: Error during unmarshalling: %v\n", err)
+	}
+
+	var updCustomerEntriesResponse []UpdCustomersResponse = make([]UpdCustomersResponse, len(updCustomerEntries))
+
+	for j := 0; j < len(updCustomerEntries); j++ {
+		var found bool
+		for i := 0; i < len(customerDb); i++ {
+			if customerDb[i].Id.String() == updCustomerEntries[j].Id.String() {
+				customerDb[i].Contacted = updCustomerEntries[j].Contacted
+				customerDb[i].Email = updCustomerEntries[j].Email
+				customerDb[i].Name = updCustomerEntries[j].Name
+				customerDb[i].Role = updCustomerEntries[j].Role
+				customerDb[i].Phone = updCustomerEntries[j].Phone
+				found = true
+
+				updCustomerEntriesResponse[j].Customerentry = updCustomerEntries[j]
+				updCustomerEntriesResponse[j].Statuscode = http.StatusOK
+				break
+			}
+		}
+		if !found {
+			updCustomerEntriesResponse[j].Customerentry = updCustomerEntries[j]
+			updCustomerEntriesResponse[j].Statuscode = http.StatusNotFound
+		}
+	}
+
+	w.WriteHeader(http.StatusOK)
+	err = json.NewEncoder(w).Encode(updCustomerEntriesResponse)
+	if err != nil {
+		fmt.Printf("UpdateCustomers: Error during unmarshalling: %v\n", err)
+	}
+}
+
+func senderrormessage(w http.ResponseWriter, message string, errorcode int) {
+	var errormsg = new(ErrorResponse)
+	errormsg.Errormessage = message
+	w.WriteHeader(errorcode)
+	json.NewEncoder(w).Encode(errormsg)
+}
+
+func main() {
+	// Instantiate a new route
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", index).Methods("GET")
+	router.HandleFunc("/customers/{id}", getCustomer).Methods("GET")
+	router.HandleFunc("/customers", getCustomers).Methods("GET")
+	router.HandleFunc("/customers", addCustomer).Methods("POST")
+	router.HandleFunc("/customers/{id}", updateCustomer).Methods("PATCH", "PUT")
+	router.HandleFunc("/customers", updateCustomers).Methods("PATCH", "PUT")
+	router.HandleFunc("/customers/{id}", deleteCustomer).Methods("DELETE")
+
+	fmt.Println("Server is starting on port 3000...")
+
+	http.ListenAndServe(":3000", router)
+}
